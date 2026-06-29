2588494e816169d59f34a80e3b5f66c9d23b945e
sadoqatyarmatova <armatovmuhamatzon@gmail.com>
2026-06-29 14:28:21 -0400

    Tag'iyrot

--- sadoqat.js
+++ sadoqat.js
@@ -1,33 +1,7 @@
-
 const API = "https://63849dde3fa7acb14ffada13.mockapi.io/api/Courses";
 
-const params = new URLSearchParams(window.location.search);
-const id = params.get("id");
+function save() {
 
-if (id) {
-    fetch(API + "/" + "id" )
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(course){
-            document.getElementById("nazvanie").value   = course.name       || "";
-            document.getElementById("opisanie").value   = course.description|| "";
-            document.getElementById("logotip").value    = course.logo       || "";
-            document.getElementById("sertifikat").value = course.sertifikat || "";
-            document.getElementById("moduli").value     = course.module     || "";
-            document.getElementById("uroki").value      = course.lessons    || "";
-            document.getElementById("uchitel").value    = course.teacher    || "";
-            document.getElementById("avatar").value     = course.avatar     || "";
-            document.getElementById("kategoriya").value = course.category   || "";
-            document.getElementById("status").value     = course.status     || "";
-        })
-
-        .catch(function(){
-            alert("Kurs va Malumotho pur karda nashud!")
-        });
-}
-
-function save (){
     const data = {
         name:        document.getElementById("nazvanie").value.trim(),
         description: document.getElementById("opisanie").value.trim(),
@@ -41,49 +15,30 @@ function save (){
         status:      document.getElementById("status").value.trim(),
     };
 
-
-    if (!data.name){
-        alert("Nomi Kursro vorid kuned!");
+    if (!data.name) {
+        alert("Nomi kursro vorid kuned!");
         document.getElementById("nazvanie").focus();
         return;
     }
 
-    if (id) {
-        fetch (API + "/" + id,{
-            method: "PUT",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(data)
-        })
-
-            .then(function(){
-                alert("Kurs bo Muvafaqiyat vorid Karda shud!")
-                window.location.href = "./index.html";
-            })
-            .catch(function(){
-                alert("Xatogi! Kurs vorid Nashud.")
-            })
-
-    }
-    else {
-        fetch(API,{
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data)
-        })
-        .then(function(response){
-            return response.json()
-        })
-        .then(function() {
-        alert("Yangi kurs qo'shildi!");
+    fetch(API, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function() {
+        alert("Kursi nav vorid shud!");
         window.location.href = "./index.html";
-         
-        })
-          .catch(function() {
-             alert("Xatolik! Kurs qo'shilmadi.");
-       });
-    }
+    })
+    .catch(function() {
+        alert("Xato! Kurs vorid nashud.");
+    });
+
 }
 
 function otmenit() {
-  window.location.href = "./index.html";
+    window.location.href = "./index.html";
 }
